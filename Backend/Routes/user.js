74152c505c68684d234ce0f94ae79238594d8e15
Piyush Kumar <piyush1671@gmail.com>
2020-08-15 17:29:14 +0530

    .

--- Backend/Routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../Controller/auth");
-
-router.get("/sessions", auth.session);
-
-module.exports = router;
