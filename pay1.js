1a9cd441181e0d3bb511d684cf9325f169b68ea3
Silverpoision <piyush1671@gmail.com>
2020-09-21 16:13:21 +0530

    added

--- pay1.js
+++ pay1.js
@@ -2,23 +2,23 @@ var stripe = Stripe(
   "pk_live_51HFZcGF48xi6aQtzPhgX6qRHq4XuNk26n11T8VvIsmSIzi4MyVFKgI1TtCHljrLf5yMeqR9ezFe4A78J40fwyxk800QpWRsrGx"
 );
 
-var checkoutButton = document.getElementById("checkout-button");
+var checkoutButton = document.getElementById("checkout-button1");
 const checkbox = document.getElementById("checkbb");
 const country = document.querySelector(".con");
 
-let price = "price_1HGOryF48xi6aQtzPrMrCYGV";
+let price = "price_1HTmRIF48xi6aQtzjULlG6bh";
 
 checkoutButton.addEventListener("click", async function (e) {
   e.preventDefault();
 
   if (country.value.toLowerCase() === "india" && checkbox.checked) {
-    price = "price_1HGOryF48xi6aQtzOp1nvimU";
+    price = "price_1HTmRJF48xi6aQtzXJ8ZHFhq";
   } else if (country.value.toLowerCase() !== "india" && checkbox.checked) {
-    price = "price_1HGOryF48xi6aQtzgQqPA89S";
+    price = "price_1HTmRJF48xi6aQtzxbYu7Kxe";
   } else if (country.value.toLowerCase() === "india" && !checkbox.checked) {
-    price = "price_1HGOryF48xi6aQtzG3GGny7Y";
+    price = "price_1HTmRIF48xi6aQtzjULlG6bh";
   } else if (country.value.toLowerCase() !== "india" && !checkbox.checked) {
-    price = "price_1HGOryF48xi6aQtzPrMrCYGV";
+    price = "price_1HTmRJF48xi6aQtzoWhsIEZI";
   }
 
   const { error } = await stripe.redirectToCheckout({
@@ -39,7 +39,7 @@ checkoutButton.addEventListener("click", async function (e) {
   }
 });
 
-const handlePotli = () => {
+const handlePotli2 = () => {
   var img = document.getElementById("img_p");
   const potli = document.querySelector("#potli");
   const gst = document.querySelector(".gst");
@@ -48,20 +48,20 @@ const handlePotli = () => {
 
   if (img.style.display === "none") {
     potli.style.display = "flex";
-    gst.innerText = "Rs. 6240";
+    gst.innerText = "Rs. 5,880";
     if (country.value.toLowerCase() !== "india") {
-      total.innerText = "Rs. 59740";
+      total.innerText = "Rs. 56,380.00";
     } else {
-      total.innerText = "Rs. 58740";
+      total.innerText = "Rs. 55,380.00";
     }
     img.style.display = "block";
   } else {
     potli.style.display = "none";
-    gst.innerText = "Rs. 5100";
+    gst.innerText = "Rs. 4,740";
     if (country.value.toLowerCase() !== "india") {
-      total.innerText = "Rs. 49100";
+      total.innerText = "Rs. 45,740.00";
     } else {
-      total.innerText = "Rs. 48100";
+      total.innerText = "Rs. 44,740.00";
     }
 
     img.style.display = "none";
@@ -69,7 +69,6 @@ const handlePotli = () => {
 };
 
 const save = (obj) => {
-  console.log("s");
   localStorage.setItem(obj.name, obj.value);
   if (obj.name === "country") {
     if (obj.value.toLowerCase() !== "india") {
