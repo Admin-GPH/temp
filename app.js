bf99688f2bd453aff445310cd1515074aee8bb20
Silverpoision <piyush1671@gmail.com>
2020-09-22 11:12:09 +0530

    .

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const color = () => {
   const color = document.querySelector(".color");
+  const color1 = document.querySelector(".color1");
   if (color.style.display === "flex") {
     color.style.display = "none";
+    color1.style.display = "none";
   } else {
     color.style.display = "flex";
+    color1.style.display = "flex";
   }
 };
